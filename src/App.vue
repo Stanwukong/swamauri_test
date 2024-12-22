a93ef92f08e7aa2f57fbc0a74048d0afad7d8ead
Oluwafemi S.E. Ajanaku <111810384+Stanwukong@users.noreply.github.com>
2024-12-22 03:10:02 +0100

    Push final

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Carousel } from "@swarmakit/vue";
+
+const slides = [
+  { src: "/img1.jpg", alt: "Image 1" },
+  { src: "/img3.jpg", alt: "Image 2" },
+  { src: "/img3.jpg", alt: "Image 2" },
+];
+
+const carouselInterval = 5000;
+</script>
+
+<template>
+  <div>
+    <Carousel :slides="slides" :interval="carouselInterval" />
+  </div>
+</template>
+
+<style scoped></style>
